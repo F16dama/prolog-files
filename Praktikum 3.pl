e62d25b9aa6fda8bb3c7c31b9f61627030689718
Denis Behrends <github@sata300.de>
2017-07-15 11:59:53 +0200

    Praktikum5.pl unfertig! Der rest fertig.

--- Praktikum 3.pl
+++ Praktikum 3.pl
@@ -3,54 +3,69 @@
 
 % Hilfsfunktionen
 
+% Rs ist Xs konkatiniert mit Ys
 append([],Xs,Xs).
 append([X|Xs], Ys, [X|Rs]):- append(Xs,Ys,Rs).
 
+% Ys ist Xs mit hinten angefügtem X
+attach(Xs,X,Ys):-  append(Xs,[X],Ys).
+
+% Ys ist Xs rückwärts
 reverse([],[]).
 reverse([X|Xs],Ys) :- reverse(Xs,Hs),append(Hs,[X],Ys).
 
-präfix([],Xs).
+% Xs ist in Ys enthalten von vorne
+präfix([],_).
 präfix([X|Xs],[X|Ys]):- präfix(Xs,Ys).
 
 
 
 % Aufgabe 1))
 
+% Ys endet mit der Liste Xs
 postfix(Xs,Ys):- append(_,Xs,Ys).
-
 postfix(Xs,Ys):- reverse(Xs,Xrs), reverse(Ys,Yrs), präfix(Xrs,Yrs).
 
 
 
 % Aufgabe 2))
 
+% biärbaum ist durch e = null und dem n(W,Lb,Rb) Funktor definiert
+% W ist Wurzel
+% Lb ist linker Teilbaum
+% Rb ist rechter Teilbaum
 binbaum(e).
-binbaum(n(X,Lb,Rb)):-
+binbaum(n(_,Lb,Rb)):-
                     binbaum(Lb),
                     binbaum(Rb).
 
+% X ist im Baum enthalten
 member(X,n(X,Lb,Rb)):- binbaum(Lb), binbaum(Rb).
 member(X,n(_,Lb,Rb)):- binbaum(Lb), binbaum(Rb), member(X,Lb).
 member(X,n(_,Lb,Rb)):- binbaum(Rb), binbaum(Lb), member(X,Rb).
 
 % Aufgabe 3
 
+% Baum wird in preorder in die Liste Ys gespeichert
+präorder(e,[]).
 präorder(n(X,Xlb,Xrb), Ys):-
                        präorder(Xlb,Yls),
                        präorder(Xrb,Yrs),
                        append([X|Yls],Yrs,Ys).
-präorder(e,[]).
 
+% Baum wird in postorder in die Liste Ys gespeichert
+postorder(e,[]).
 postorder(n(X,Xlb,Xrb), Ys):-
                        postorder(Xlb,Yls),
                        postorder(Xrb,Yrs),
                        append(Yls,Yrs,Ts), append(Ts,[X],Ys).
-postorder(e,[]).
 
+% Y ist Wurzel von Baum
 root(n(Y,Lb,Rb),Y):- binbaum(n(Y,Lb,Rb)).
 
+% Von allen Rb die Wurzel wird in Ys gespeichert
 roots([],[]).
-roots([e|Xs],Ys):- roots(Xs,Yns).
+roots([e|Xs],Ys):- roots(Xs,Ys).
 roots([Rb|Xs],Ys):- root(Rb,X), roots(Xs,Yns), append([X],Yns,Ys).
 
 
---
+++ Praktikum 5,1.pl
@@ -0,0 +1,69 @@
+﻿% Autor:
+% Datum: 04.06.2017
+
+
+%a)
+
+%Startzustand
+start(z0).
+
+%Endzustand
+ende(#).
+
+%Zustände
+z(z0).
+z(z1).
+
+
+%Alphabet Sigma
+sigma(0).
+sigma(1).
+sigma($).
+
+%Alphabet Keller
+keller(1).
+keller(0).
+keller(#).
+
+%Transrelationen
+delta(z0, $, #, z1, [#]).
+delta(z0, $, 0, z1, [0]).
+delta(z0, $, 1, z1, [1]).
+
+delta(z0, 1, #, z0, [1,#]).
+delta(z0, 1, 0, z0, [1,0]).
+delta(z0, 1, 1, z0, [1,1]).
+
+delta(z0, 0, #, z0, [0,#]).
+delta(z0, 0, 0, z0, [0,0]).
+delta(z0, 0, 1, z0, [0,1]).
+
+delta(z1, 1, 1, z1, []).
+delta(z1, 0, 0, z1, []).
+delta(z1, nix, #, z1, []).
+
+% Sigma*
+sigma_stern([]).
+sigma_stern([X|Xs]) :- sigma(X), sigma_stern(Xs).
+
+%Einzelschrittrelationen
+%(z, aw, gs, z', w, s's) ∈ |-M if (z, a, g, z', s') ∈ ∂
+es(S, [A|Ws], [T | Gs], NS, Ws, NKs) :-
+    delta(S, A, T, NS, TKs),
+    append(TKs, Gs, NKs).
+
+%(z, w, gs, z', w, s's) ∈ |-M if (z, nix, g, z', s') ∈ ∂
+es(S, Ws, [T|Gs], NS, Ws, NKs) :-
+    delta(S, nix, T, NS, TKs),
+    append(TKs, Gs, NKs).
+
+%Transitiver Abschluss Einzelschrittrelationen
+es_plus(_, [], [Es]) :- ende(Es).
+es_plus(S, Ws, Ks) :-
+    es(S, Ws, Ks, NS, NWs, NKs),
+    es_plus(NS, NWs, NKs).
+    
+    
+% Relation lvonM(Ws) die allgemein der Sprache
+% entspricht, die von definierten PDA erkannt wird
+lvonM(Xs) :- start(S), z(S), ende(H), keller(H), es_plus(S, Xs, [H]).
